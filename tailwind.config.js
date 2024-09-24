f35eb38bbf6f961f0c87765d6c6dc635f15779be
fxjakaria04 <freelancerx2023@gmail.com>
2024-09-24 10:35:49 +0600

    some work done

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ['application.html', 'index.html', 'about.html', 'partnership.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
