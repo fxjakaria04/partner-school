29f18926e638277f8ecdf9d8b127cd2f19a531e6
fxjakaria04 <freelancerx2023@gmail.com>
2024-09-25 14:49:18 +0600

    billing page added and some design done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ['application.html', 'index.html', 'about.html', 'partnership.html', 'product.html'],
+  content: ['application.html', 'index.html', 'about.html', 'partnership.html', 'product.html', 'billing.html'],
   theme: {
     extend: {},
   },
