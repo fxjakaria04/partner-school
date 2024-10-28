a649c6fac684a88ec5007e0c14c405518ef92028
fxjakaria04 <freelancerx2023@gmail.com>
2024-10-28 12:17:03 +0600

    added all products page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ['application.html', 'index.html', 'about.html', 'partnership.html', 'product.html', 'billing.html'],
+  content: ['application.html', 'index.html', 'about.html', 'partnership.html', 'product.html', 'billing.html', 'table.html'],
   theme: {
     extend: {},
   },
